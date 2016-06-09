cdb16b610be46c4b132edad61108faaec489d018
Wade Wegner <wade.wegner@gmail.com>
2016-06-09 08:38:19 -0700

    Removed comment

--- src/Salesforce.Metadata.Api/MetadataClient.cs
+++ src/Salesforce.Metadata.Api/MetadataClient.cs
@@ -13,33 +13,6 @@ namespace Salesforce.CSharp.Metadata
 {
     public class MetadataClient
     {
-//POST https://na30.salesforce.com/services/Soap/m/31.0/00D36000000vx1P HTTP/1.1
-//Accept-Encoding: identity
-//Content-Length: 670
-//Soapaction: ""
-//Host: na30.salesforce.com
-//User-Agent: MavensMate
-//Connection: close
-//Content-Type: text/xml; charset=utf-8
-
-//<?xml version="1.0" encoding="UTF-8"?>
-//<SOAP-ENV:Envelope xmlns:ns0="http://soap.sforce.com/2006/04/metadata" 
-//xmlns:ns1="http://schemas.xmlsoap.org/soap/envelope/"
-//xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
-//xmlns:tns="http://soap.sforce.com/2006/04/metadata" 
-//xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">
-//<SOAP-ENV:Header>
-//<tns:SessionHeader>
-//<tns:sessionId>00D36000000vx1P!ARYAQEpkgv1L1cdeRLuWtboG4371wO561g6gotzO.Pr3X67aJKW6bDA8QWWH.s5cJQolBWWY6XZTEKCHxgeNavIQouMTM5mp</tns:sessionId>
-//</tns:SessionHeader>
-//</SOAP-ENV:Header>
-//<ns1:Body>
-//<ns0:describeMetadata>
-//<ns0:asOfVersion>31.0</ns0:asOfVersion>
-//</ns0:describeMetadata>
-//</ns1:Body>
-//</SOAP-ENV:Envelope>
-
         public async Task<dynamic> DescribeMetadata(string url, string sessionId, string apiVersion)
         {
             var soap = string.Format(@"
